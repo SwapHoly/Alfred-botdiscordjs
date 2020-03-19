1f0f96aedf67316ceea44334870f316b260f426b
SwapHoly <airaudthomas@laposte.net>
2020-03-19 23:33:58 +0100

    Ajout API

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ client.commands = new Collection();
 client.commands.set("repeat", require("./commands/repeat.js"));
 client.commands.set("role", require("./commands/role.js"));
 client.commands.set("sinfo", require("./commands/sinfo.js"));
+client.commands.set("animals", require("./commands/animals.js"));
 
 client.on("ready", () => require("./events/ready.js")(client));
 client.on("message", message => require("./events/message.js")(client, message));
